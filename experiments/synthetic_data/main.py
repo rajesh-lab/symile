1139ed1fac3ffa597b489c15fd7c5047e1c6da3b
Adriel Saporta <adrielsaporta@adriels-mbp.myfiosgateway.com>
2023-06-22 22:21:40 -0400

    quick test

--- experiments/synthetic_data/main.py
+++ experiments/synthetic_data/main.py
@@ -1,6 +1,7 @@
 """
 Experiment to demonstrate performance of SYMILE on synthetic data.
 """
+from sklearn.linear_model import LinearRegression
 import torch
 from torch.utils.data import DataLoader
 try:
@@ -14,10 +15,12 @@ from models import LinearEncoders
 from params import parse_args, wandb_init
 
 
-def load_data(d, n, batch_size):
-    pt_dataset = PretrainingDataset(d, n)
-    pt_loader = DataLoader(pt_dataset, batch_size=batch_size, shuffle=True)
-    ft_loader = None
+def load_data(args):
+    pt_dataset = PretrainingDataset(args.d_v, args.pretrain_n)
+    pt_loader = DataLoader(pt_dataset, batch_size=args.batch_sz, shuffle=True)
+
+    ft_dataset = PretrainingDataset(args.d_v, args.finetune_n)
+    ft_loader = DataLoader(ft_dataset, batch_size=args.batch_sz, shuffle=True)
     return pt_loader, ft_loader
 
 def pretrain(pt_loader, model, loss_fn, optimizer, args):
@@ -33,13 +36,42 @@ def pretrain(pt_loader, model, loss_fn, optimizer, args):
                 wandb.log({"loss": loss, "logit_scale": model.logit_scale.item()})
             optimizer.zero_grad()
 
+def finetune(ft_loader, model, args):
+    model.eval()
+    X_train = None
+    y_train = None
+    X_test = None
+    y_test = None
+    with torch.no_grad():
+        i = 0
+        for v_a, v_b, v_c in ft_loader:
+            r_a, r_b, r_c, logit_scale = model(v_a, v_b, v_c)
+            if i == 0:
+                X_test = torch.cat((r_a,r_b), dim=1)
+                y_test = r_c
+            else:
+                if X_train is None:
+                    X_train = torch.cat((r_a,r_b), dim=1)
+                else:
+                    X_train = torch.cat((X_train, torch.cat((r_a,r_b), dim=1)), dim=0)
+                if y_train is None:
+                    y_train = r_c
+                else:
+                    y_train = torch.cat((y_train, r_c), dim=0)
+            i += 1
+    reg = LinearRegression().fit(X_train, y_train)
+    print("R^2 score: {}".format(reg.score(X_test, y_test)))
+
+
 if __name__ == '__main__':
     # TODO: write tests for all experiment scripts
     args = parse_args()
     wandb_init(args)
 
-    pt_loader, ft_loader = load_data(args.d_v, args.n, args.batch_sz)
+    pt_loader, ft_loader = load_data(args)
     model = LinearEncoders(args.d_v, args.d_r)
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr)
     loss_fn = symile if args.loss_fn == "symile" else pairwise_infonce
     pretrain(pt_loader, model, loss_fn, optimizer, args)
+
+    reg = finetune(ft_loader, model, args)
--- experiments/synthetic_data/params.py
+++ experiments/synthetic_data/params.py
@@ -6,11 +6,15 @@ except ImportError:
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n", type=int, default=100,
-                        help="Number of samples triples (a, b, c) in generated dataset.")
+    parser.add_argument("--pretrain_n", type=int, default=1000,
+                        help="Number of samples (a, b, c) in pretraining dataset.")
+    parser.add_argument("--finetune_n", type=int, default=1000,
+                        help="Number of samples (a, b, c) in finetuning dataset.")
+    parser.add_argument("--test_n", type=int, default=100,
+                        help="Number of samples (a, b, c) in test dataset.")
     parser.add_argument("--d_v", type=int, default=10,
                         help="Dimensionality of dataset vectors.")
-    parser.add_argument("--d_r", type=int, default=2,
+    parser.add_argument("--d_r", type=int, default=1,
                         help="Dimensionality of representation vectors.")
     parser.add_argument("--batch_sz", type=int, default=32,
                         help="Batch size for training.")
@@ -20,7 +24,7 @@ def parse_args():
                         help="Loss function to use for training. Options are 'symile' and 'pairwise_infonce'.")
     parser.add_argument("--epochs", type=int, default=32,
                         help="Number of epochs to train for.")
-    parser.add_argument("--normalize", type=bool, default=True,
+    parser.add_argument("--normalize", type=bool, default=False,
                         help="Whether to normalize representations before loss calculation.")
     parser.add_argument("--wandb", type=bool, default=False,
                         help="Whether to use wandb for logging.")
