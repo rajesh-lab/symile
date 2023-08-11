45aa337e7c86d7c0f7c97eea0f8d545de62d0e7e
Adriel Saporta <as16583@log-3.hpc.nyu.edu>
2023-08-10 22:07:25 -0400

    add parameters for hardcoding encoders and initializing temperature param

--- src/synthetic_experiments/models.py
+++ src/synthetic_experiments/models.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 
 class LinearEncoders(nn.Module):
-    def __init__(self, d_v, d_r):
+    def __init__(self, d_v, d_r, logit_scale_init, hardcode_encoders):
         """
         Initialize linear encoders that generate representations r_a, r_b, r_c
         from input vectors v_a, v_b, v_c.
@@ -12,6 +12,7 @@ class LinearEncoders(nn.Module):
         Args:
             d_v (int): dimensionality for each of the vectors v_a, v_b, v_c.
             d_r (int): dimensionality for each of the representations r_a, r_b, r_c.
+            logit_scale_init (float): initial value for logit_scale parameter.
         """
         super().__init__()
         self.d_v = d_v
@@ -19,9 +20,19 @@ class LinearEncoders(nn.Module):
         self.f_a = nn.Linear(d_v, d_r, bias=True)
         self.f_b = nn.Linear(d_v, d_r, bias=True)
         self.f_c = nn.Linear(d_v, d_r, bias=True)
-        # temperature parameter used by CLIP:
+        if hardcode_encoders:
+            with torch.no_grad():
+                weight_ab = torch.zeros(d_r, d_v).fill_diagonal_(2)
+                self.f_a.weight = nn.Parameter(weight_ab, requires_grad=False)
+                self.f_b.weight = nn.Parameter(weight_ab, requires_grad=False)
+                self.f_a.bias = nn.Parameter(torch.full((d_r,), -1.0), requires_grad=False)
+                self.f_b.bias = nn.Parameter(torch.full((d_r,), -1.0), requires_grad=False)
+                weight_c = torch.zeros(d_r, d_v).fill_diagonal_(-2)
+                self.f_c.weight = nn.Parameter(weight_c, requires_grad=False)
+                self.f_c.bias = nn.Parameter(torch.full((d_r,), 1.0), requires_grad=False)
+        # temperature parameter is learned as done by CLIP:
         # https://github.com/openai/CLIP/blob/a1d071733d7111c9c014f024669f959182114e33/clip/model.py#L295
-        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        self.logit_scale = nn.Parameter(torch.ones([]) * logit_scale_init)
 
     def forward(self, v_a, v_b, v_c=None):
         """
--- src/synthetic_experiments/params.py
+++ src/synthetic_experiments/params.py
@@ -8,17 +8,15 @@ except ImportError:
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--evaluation", type=str,
-                        default="sum_clf",
+                        default="support_clf",
                         help="Evaluation methods to run. Must be comma-separated. \
                               Options are: zeroshot_clf, support_clf, sum_clf.")
-    parser.add_argument("--pretrain_n", type=int, default=20,
+    parser.add_argument("--pretrain_n", type=int, default=5000,
                         help="Number of samples (a, b, c) in pretraining dataset.")
-    parser.add_argument("--pretrain_val_n", type=int, default=3,
+    parser.add_argument("--pretrain_val_n", type=int, default=1000,
                         help="Number of samples (a, b, c) in pretraining validation dataset.")
     parser.add_argument("--early_stopping_patience_pt", type=int, default=4,
                         help="Number of times val loss must improve in a row before stopping pretraining.")
-    parser.add_argument("--finetune_n", type=int, default=10000,
-                        help="Number of samples (a, b, c) in finetuning dataset.")
     parser.add_argument("--test_n", type=int, default=10,
                         help="Number of samples (a, b, c) in test dataset.")
     parser.add_argument("--d_v", type=int, default=2,
@@ -34,17 +32,28 @@ def parse_args():
     parser.add_argument("--lr", type=float, default=1.0e-2,
                         help="Learning rate.")
     parser.add_argument("--loss_fn", type=str, default="symile",
-                        help="Loss function to use for training. Options are 'symile' and 'pairwise_infonce'.")
+                        help="Loss function to use for training. \
+                              Options are 'symile' and 'pairwise_infonce'.")
     parser.add_argument("--concat_infonce", type=bool, default=False,
                         help="Whether or not to concatenate (r_a * r_b), (r_b * r_c), (r_a * r_c) for \
                               downstream classification tasks when loss function is 'pairwise_infonce' \
                               (alternative is to sum the three terms).")
-    parser.add_argument("--pt_epochs", type=int, default=10,
+    parser.add_argument("--use_temp_param_eval", type=bool, default=False,
+                        help="Whether or not to scale logits by temperature \
+                              parameter during evaluation.")
+    parser.add_argument("--pt_epochs", type=int, default=1,
                         help="Number of epochs to pretrain for.")
-    parser.add_argument("--pt_val_epochs", type=int, default=2,
+    parser.add_argument("--pt_val_epochs", type=int, default=10,
                         help="Number of epochs after which to calculate pretraining val loss.")
     parser.add_argument("--normalize", type=bool, default=True,
                         help="Whether to normalize representations before loss calculation.")
+    parser.add_argument("--normalize_eval", type=bool, default=False,
+                        help="Whether to normalize representations during evaluation.")
+    parser.add_argument("--logit_scale_init", type=float, default=2.65926,
+                        help="Value used to initialize the learned logit_scale. \
+                              CLIP used np.log(1 / 0.07) = 2.65926.")
+    parser.add_argument("--hardcode_encoders", type=bool, default=True,
+                        help="Whether to hardcode encoders during training.")
     parser.add_argument("--wandb", type=bool, default=False,
                         help="Whether to use wandb for logging.")
     return parser.parse_args()
