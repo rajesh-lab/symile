f9f3de8fdc974c691ab63923884c5a6ac46d5ec5
Adriel Saporta <as16583@log-3.hpc.nyu.edu>
2023-08-16 14:47:34 -0400

    add str_to_bool to params, add use_seed

--- src/synthetic_experiments/params.py
+++ src/synthetic_experiments/params.py
@@ -1,65 +1,58 @@
 import argparse
-try:
-    import wandb
-except ImportError:
-    wandb = None
+from utils import str_to_bool
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--evaluation", type=str,
+                        choices=["zeroshot_clf", "support_clf", "sum_clf"],
                         default="support_clf",
-                        help="Evaluation methods to run. Must be comma-separated. \
-                              Options are: zeroshot_clf, support_clf, sum_clf.")
-    parser.add_argument("--pretrain_n", type=int, default=5000,
+                        help="Evaluation methods to run. Must be comma-separated.")
+    parser.add_argument("--pretrain_n", type=int, default=20000,
                         help="Number of samples (a, b, c) in pretraining dataset.")
     parser.add_argument("--pretrain_val_n", type=int, default=1000,
                         help="Number of samples (a, b, c) in pretraining validation dataset.")
     parser.add_argument("--early_stopping_patience_pt", type=int, default=4,
                         help="Number of times val loss must improve in a row before stopping pretraining.")
-    parser.add_argument("--test_n", type=int, default=10,
+    parser.add_argument("--test_n", type=int, default=5000,
                         help="Number of samples (a, b, c) in test dataset.")
     parser.add_argument("--d_v", type=int, default=2,
                         help="Dimensionality of dataset vectors.")
     parser.add_argument("--d_r", type=int, default=2,
                         help="Dimensionality of representation vectors.")
-    parser.add_argument("--use_full_dataset", type=bool, default=True,
+    parser.add_argument("--use_full_dataset", type=str_to_bool, default=True,
                         help="Whether to set batch size equal to full dataset. \
                               Note that if set to True, `batch_sz` param below \
                               will be ignored.")
-    parser.add_argument("--batch_sz_pt", type=int, default=5000,
+    parser.add_argument("--batch_sz_pt", type=int, default=20000,
                         help="Batch size for pretraining.")
-    parser.add_argument("--lr", type=float, default=1.0e-2,
+    parser.add_argument("--lr", type=float, default=1.0e-1,
                         help="Learning rate.")
-    parser.add_argument("--loss_fn", type=str, default="symile",
-                        help="Loss function to use for training. \
-                              Options are 'symile' and 'pairwise_infonce'.")
-    parser.add_argument("--concat_infonce", type=bool, default=False,
+    parser.add_argument("--loss_fn", type=str,
+                        choices = ["symile", "pairwise_infonce"], default="pairwise_infonce",
+                        help="Loss function to use for training.")
+    parser.add_argument("--concat_infonce", type=str_to_bool, default=False,
                         help="Whether or not to concatenate (r_a * r_b), (r_b * r_c), (r_a * r_c) for \
                               downstream classification tasks when loss function is 'pairwise_infonce' \
                               (alternative is to sum the three terms).")
-    parser.add_argument("--use_temp_param_eval", type=bool, default=False,
+    parser.add_argument("--use_logit_scale_eval", type=str_to_bool, default=True,
                         help="Whether or not to scale logits by temperature \
                               parameter during evaluation.")
-    parser.add_argument("--pt_epochs", type=int, default=1,
+    parser.add_argument("--pt_epochs", type=int, default=160,
                         help="Number of epochs to pretrain for.")
     parser.add_argument("--pt_val_epochs", type=int, default=10,
                         help="Number of epochs after which to calculate pretraining val loss.")
-    parser.add_argument("--normalize", type=bool, default=True,
-                        help="Whether to normalize representations before loss calculation.")
-    parser.add_argument("--normalize_eval", type=bool, default=False,
-                        help="Whether to normalize representations during evaluation.")
-    parser.add_argument("--logit_scale_init", type=float, default=2.65926,
+    parser.add_argument("--normalize", type=str_to_bool, default=True,
+                        help="Whether to normalize representations, both during \
+                              pre-training before loss calculation and during evaluation.")
+    parser.add_argument("--logit_scale_init", type=float, default=-0.3,
                         help="Value used to initialize the learned logit_scale. \
                               CLIP used np.log(1 / 0.07) = 2.65926.")
-    parser.add_argument("--hardcode_encoders", type=bool, default=True,
+    parser.add_argument("--hardcode_encoders", type=str_to_bool, default=False,
                         help="Whether to hardcode encoders during training.")
-    parser.add_argument("--wandb", type=bool, default=False,
+    parser.add_argument("--use_seed", type=str_to_bool, default=True,
+                        help="Whether to use a seed for reproducibility.")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--wandb", type=str_to_bool, default=True,
                         help="Whether to use wandb for logging.")
     return parser.parse_args()
-
-def wandb_init(args):
-    if args.wandb:
-        wandb.init(project="symile",
-                   config=args)
-    return
