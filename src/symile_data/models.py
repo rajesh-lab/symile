f46c1566d63adee06c01098916b5ebb90b317f4c
Adriel Saporta <as16583@log-3.hpc.nyu.edu>
2023-09-07 18:26:37 -0400

    Add support classification model module

--- src/symile_data/models.py
+++ src/symile_data/models.py
@@ -4,8 +4,13 @@ import lightning.pytorch as pl
 import torch
 import torch.nn as nn
 from transformers import BertModel, CLIPVisionModel, WhisperModel, XLMRobertaModel
+try:
+    import wandb
+except ImportError:
+    wandb = None
 
 from src.losses import pairwise_infonce, symile
+from utils import l2_normalize
 
 
 class ProjectionHead(nn.Module):
@@ -132,18 +137,115 @@ class SymileModel(pl.LightningModule):
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.args.lr)
         return optimizer
 
-    def training_step(self, batch, batch_idx):
+    def _shared_step(self, batch, batch_idx):
         r_a, r_i, r_t, logit_scale_exp = self(batch)
-        loss = self.loss_fn(r_a, r_i, r_t, logit_scale_exp, self.args.normalize)
+
+        if self.args.normalize:
+            r_a, r_i, r_t = l2_normalize([r_a, r_i, r_t])
+
+        return self.loss_fn(r_a, r_i, r_t, logit_scale_exp)
+
+    def training_step(self, batch, batch_idx):
+        loss = self._shared_step(batch, batch_idx)
         self.log_dict({"train_loss": loss, "logit_scale_exp": logit_scale_exp},
-                      sync_dist=True, prog_bar=True)
+                      on_step=False, on_epoch=True, sync_dist=True, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        r_a, r_i, r_t, logit_scale_exp = self(batch)
-        loss = self.loss_fn(r_a, r_i, r_t, logit_scale_exp, self.args.normalize)
-        self.log("val_loss", loss, sync_dist=True, prog_bar=True)
+        loss = self._shared_step(batch, batch_idx)
+        self.log("val_loss", loss, on_step=False, on_epoch=True,
+                 sync_dist=True, prog_bar=True)
         return loss
 
-    def test(self, batch, batch_idx):
-        pass
+
+class SupportClfModel(pl.LightningModule):
+    def __init__(self, **args):
+        super().__init__()
+        self.args = Namespace(**args)
+        for k in args.keys():
+            self.save_hyperparameters(k)
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = SymileModel.load_from_checkpoint(self.args.ckpt_path,
+                                                      map_location=device)
+        self.model.freeze()
+        self.audio_encoder = self.model.audio_encoder
+        self.image_encoder = self.model.image_encoder
+        self.text_encoder = self.model.text_encoder
+
+        in_features = self.text_encoder.projection_head.linear_projection.out_features
+        self.classifier = nn.Linear(in_features, 1, bias=True)
+
+        self.training_step_outputs = {"y": [], "prob": []}
+
+    def forward(self, x):
+        r_a = self.audio_encoder(x["audio_input_features"], x["audio_attention_mask"])
+        r_i = self.image_encoder(x["image_pixel_values"])
+        r_t = self.text_encoder(x["text_input_ids"], x["text_attention_mask"])
+
+        if self.model.args.normalize:
+            r_a, r_i, r_t = l2_normalize([r_a, r_i, r_t])
+
+        if self.model.args.loss_fn == "symile":
+            X = r_a * r_i * r_t
+        elif self.model.args.loss_fn == "pairwise_infonce":
+            if self.args.concat_infonce:
+                X = torch.cat((r_a * r_i, r_i * r_t, r_a * r_t), dim=1)
+            else:
+                X = (r_a * r_i) + (r_i * r_t) + (r_a * r_t)
+
+        if self.args.use_logit_scale:
+            X = self.model.logit_scale.exp() * X
+
+        return self.classifier(X)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.parameters(), lr=self.args.lr)
+        return optimizer
+
+    def _shared_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch["in_support"].unsqueeze(1)
+        return nn.BCEWithLogitsLoss()(y_hat, y)
+
+    def training_step(self, batch, batch_idx):
+        loss = self._shared_step(batch, batch_idx)
+        self.log("train_loss", loss, on_step=False, on_epoch=True,
+                 sync_dist=True, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._shared_step(batch, batch_idx)
+        self.log("val_loss", loss, on_step=False, on_epoch=True,
+                 sync_dist=True, prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        y_hat = self(batch)
+        y = batch["in_support"].unsqueeze(1)
+        loss = nn.BCEWithLogitsLoss()(y_hat, y)
+
+        # save y and probabilities to compute accuracy at epoch end
+        prob = nn.Sigmoid()(y_hat)
+        self.training_step_outputs["prob"].append(prob)
+        self.training_step_outputs["y"].append(y.squeeze())
+
+        self.log("test_loss", loss, on_step=False, on_epoch=True,
+                 sync_dist=True, prog_bar=True)
+        return loss
+
+    def on_test_epoch_end(self):
+        prob = torch.cat(self.training_step_outputs["prob"])
+        pred = torch.where(prob >= 0.5, 1, 0).squeeze()
+        y = torch.cat(self.training_step_outputs["y"])
+        acc = (torch.sum(y == pred) / len(y)).item()
+        self.log("test_accuracy", acc, sync_dist=True, prog_bar=True)
+
+        # wandb.plot.roc_curve expects probabilities to have shape (n, n_classes)
+        if self.args.wandb:
+            y = y.cpu()
+            prob = prob.cpu()
+            probs = torch.cat([(1-prob), prob], dim=1)
+            self.logger.experiment.log({"roc_curve": wandb.plot.roc_curve(y, probs)})
+
+        self.training_step_outputs.clear() # free memory
