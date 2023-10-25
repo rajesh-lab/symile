d5f0f226e2856b7ce1ba8e9921c961a8d325a049
Adriel Saporta <adriel@nyu.edu>
2023-10-25 14:17:45 -0400

    compute_logits using all negatives

--- src/losses.py
+++ src/losses.py
@@ -40,13 +40,18 @@ def pairwise_infonce(r_a, r_b, r_c, logit_scale):
 ##########
 # symile #
 ##########
-def compute_logits(x, y, z):
+def compute_logits_efficient(x, y, z):
     """
-    Computes the logits for x, by computing the positive multilinear inner product
-    and the negative multilinear inner products for each sample (row) in x.
-    Positive multilinear inner products (MIP) are along the diagonal of the logits matrix.
-    For example, the second row of `logits` might be:
+    Computes the logits for x with only (batch_size^2 - batch_size) negatives.
+
+    If batch size is n, then returned logits have size (n, n) with n positive
+    multilinear inner products and (n^2 - n) negative multilinear inner products.
+
+    Positive multilinear inner products (MIP) are along the diagonal of the
+    square logits matrix. For example, the second row of `logits` might be:
+
     [ MIP(x[1], y[3], z[2]) MIP(x[1], y[1], z[1]) MIP(x[1], y[0], z[1]) MIP(x[1], y[2], z[3]) ].
+
     Notice that only the second element is the positive MIP; all others are negative.
     There is a small chance of a false negative MIP.
 
@@ -60,13 +65,51 @@ def compute_logits(x, y, z):
     # shuffle rows of y and z
     y_shuff = y[torch.randperm(y.shape[0])]
     z_shuff = z[torch.randperm(z.shape[0])]
-    def _multilinear_inner_product_shuffled(x):
-        return torch.t(x) @ torch.t(y_shuff * z_shuff)
-
-    logits_x = torch.vmap(_multilinear_inner_product_shuffled)(x) # (batch_sz, batch_sz)
+    logits_x = x @ torch.t(y_shuff * z_shuff) # (batch_sz, batch_sz)
     MIP_of_pos_triples = (x * y * z).sum(axis=1) # (batch_sz)
     # insert positive triples along diagonal of shuffled logits
     return torch.where(torch.eye(n=x.shape[0]).to(x.device) > 0.5, MIP_of_pos_triples, logits_x)
+
+
+def compute_logits(x, y, z):
+    """
+    Computes the logits for x.
+
+    If batch size is n, then returned logits have size (n, n^2) with n positive
+    multilinear inner products and (n^3 - n) negative multilinear inner products.
+
+    Positive multilinear inner products (MIP) are along the main diagonal of the
+    (non-square) logits matrix. For example, if n = 4, then the second row of
+    `logits` is:
+
+    [ MIP(x[1], y[0], z[0]) MIP(x[1], y[1], z[1]) MIP(x[1], y[2], z[2]) MIP(x[1], y[3], z[3])
+      MIP(x[1], y[0], z[3]) MIP(x[1], y[1], z[0]) MIP(x[1], y[2], z[1]) MIP(x[1], y[3], z[2])
+      MIP(x[1], y[0], z[2]) MIP(x[1], y[1], z[3]) MIP(x[1], y[2], z[0]) MIP(x[1], y[3], z[1])
+      MIP(x[1], y[0], z[1]) MIP(x[1], y[1], z[2]) MIP(x[1], y[2], z[3]) MIP(x[1], y[3], z[0])  ]
+
+    Notice that only the second element is the positive MIP; all others are negative.
+
+    Args:
+        x (torch.Tensor): representation vector of size (batch_size, d_r).
+        y (torch.Tensor): representation vector of size (batch_size, d_r).
+        z (torch.Tensor): representation vector of size (batch_size, d_r).
+    Returns:
+        logits (torch.Tensor): logits for x of size (batch_size, batch_size^2).
+    """
+    y_z = []
+    for i in range(y.shape[0]):
+        y_z.append(y * z)
+        z = torch.roll(z, shifts=1, dims=0)
+
+    # concatenate elements in y_z so that y_z has shape (n^2, d) where each row
+    # is a different element-wise product of a row from y and a row from z
+    y_z = torch.cat(y_z, 0)
+
+    # return logits with shape (n, n^2) where each row is the multilinear inner
+    # product between that row in x and each row from y_z
+    logits = x @ y_z.T
+    return logits
+
 
 def symile(r_a, r_b, r_c, logit_scale):
     logits_a = logit_scale * compute_logits(r_a, r_b, r_c)
