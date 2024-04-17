0dd25be37d12e5503fe4baf454082c6d5748cada
Adriel Saporta <adriel.saporta@gmail.com>
2024-04-17 14:26:45 -0400

    add wandb_run_id to wandblogger

--- symile/high_dim/main.py
+++ symile/high_dim/main.py
@@ -54,7 +54,8 @@ if __name__ == '__main__':
     print("\nSaving to: ", save_dir)
 
     if args.wandb:
-        logger = WandbLogger(project="symile", log_model=False, save_dir=args.ckpt_save_dir)
+        logger = WandbLogger(project="symile", log_model=False,
+                             save_dir=args.ckpt_save_dir, id=args.wandb_run_id)
     else:
         logger = False
 
